a909efad90d8a0b887bfea8dcb5d41cc0d0f457c
Tarun Reddy <tarutornado@gmail.com>
2023-08-27 14:52:00 +0530

    changes for heroku deployment

--- main.py
+++ main.py
@@ -9,22 +9,28 @@ from model import answer_question
 
 app = FastAPI()
 
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# load_dotenv()
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = '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'
 
-origins = [os.getenv("ORIGIN")]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# origins = [os.getenv("ORIGIN")]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 class Question(BaseModel):
     question: str
+
+
+@app.get("/")
+def get_answer():
+    return {'Data' : 'Hello User'}
 
 
 @app.post("/ask")

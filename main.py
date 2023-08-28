072cfb1eb7b1fe75d84ff0c32b76fe71dd7acbd4
Tarun Reddy <tarutornado@gmail.com>
2023-08-28 10:27:28 +0530

    Add code to serve frontend as static files.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import openai
@@ -12,6 +14,22 @@ app = FastAPI()
 load_dotenv()
 
 
+# Serve the Vue.js frontend files
+app.mount("/", StaticFiles(directory="../frontend/dist", html=True))
+
+# origins = [os.getenv("ORIGIN")]
+
+# Add CORS middleware to allow frontend API requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to limit allowed origins
+    # allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 def get_openai_api_key():
     if os.getenv('LOCAL_DEV') == 'True':
         return os.getenv("OPENAI_API_KEY")
@@ -20,15 +38,6 @@ def get_openai_api_key():
 
 
 openai.api_key = get_openai_api_key()
-# origins = [os.getenv("ORIGIN")]
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 
 class Question(BaseModel):
